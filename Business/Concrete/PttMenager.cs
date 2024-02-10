eff71cb9a100bd47455d0564e419e10f934a81aa
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2024-02-10 13:40:21 +0300

    Add project files.

---
+++ Business/Concrete/PttMenager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+   public class PttMenager : ISupplierService
+    {
+        public IApplicantService _applicantService;
+        
+        public PttMenager(IApplicantService applicantService) //Constructor metod.
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            if(_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske talebi oluşturuldu.");
+            }
+        }
+
+    }
+}
---
+++ WorkSpace/Program.cs
@@ -0,0 +1,33 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.FirstName = "Ömer Faruk";
+            person1.LastName = "Gözegir";
+
+            PttMenager pttMenager = new PttMenager(new ForeignerMenager());
+            pttMenager.GiveMask(person1);
+        
+        }
+
+       
+    }
+     
+  
+
+  
+
+
+}
